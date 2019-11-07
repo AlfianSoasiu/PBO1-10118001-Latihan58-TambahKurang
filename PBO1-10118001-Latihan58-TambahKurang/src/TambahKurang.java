f1f7a88682424699b91c44c5f2af0c88f49f119c
Alfian Soasiu <55717248+AlfianSoasiu@users.noreply.github.com>
2019-11-08 00:14:44 +0700

    Add files via upload

---
+++ PBO1-10118001-Latihan58-TambahKurang/src/TambahKurang.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama      : Alfian Soasiu
+ * Kelas     : PBO1
+ * NIM       : 10118001
+ * Deskripsi : Menampilkan output tambah kurang 
+ */
+public class TambahKurang {
+
+    public static void main(String[] args) {
+        
+        JumlahBilangan jBilangan = new JumlahBilangan();
+        jBilangan.tampilHasilJumlah();
+        
+        SelisihBilangan sBilangan = new SelisihBilangan();
+        sBilangan.tampilSelisih();
+    }
+    
+}
